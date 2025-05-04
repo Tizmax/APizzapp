8c6a4684d3b5dcecd65ba3f3cca6108076a68ed8
Spiroxx <mostfasami69@gmail.com>
2025-05-04 18:00:01 +0200

    Route entre Loading et Menu OK
    - Création du dossier feature pour toutes nos features (et donc déplacement de loading dedans)
    - Création de la feature "menu"
    
    - Pour la création d'une route,
    - Pour la création d'une route, dans la feature de départ, penser à ajouter "RouterModule" dans le champ "imports" de @Component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 // src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
+import { LandingComponent } from './features/landing/landing.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingComponent }, // page d'accueil
+  { path: '', component: LandingComponent },
+  { path: 'menu', 
+    loadChildren: () => 
+      import('./features/menu/menu.module').then((m) => m.MenuModule) }, // page d'accueil
   { path: '**', redirectTo: '' } // redirection des routes non définies
 ];
 
