c6353edb1ec4cf8946a80561009ee70d6aeab90d
Tizmax <matthis.bernardini@gmail.com>
2025-05-13 17:31:16 +0200

    ajout liste des suppléments + add/remove suppléments

--- frontend/src/app/features/menu/detail-produit/detail-produit.component.ts
+++ frontend/src/app/features/menu/detail-produit/detail-produit.component.ts
@@ -13,6 +13,7 @@ import { Ingredient } from '../../../shared/models/ingredient.model';
 export class DetailProduitComponent {
   pizza: Pizza | null = null;
   ingredients: Ingredient[] = [];
+  supplements: Ingredient[] = [];
 
   constructor(private route: ActivatedRoute, private pizzaService: PizzaService) {}
 
@@ -27,5 +28,27 @@ export class DetailProduitComponent {
     this.pizzaService.getAllIngredients().subscribe(
       (data) => this.ingredients = data
     );
+  }
+
+  addSupplement(ingredient: Ingredient): void {
+    // Logique pour ajouter un supplément
+    console.log('Supplément ajouté:', ingredient);
+    this.supplements.push(ingredient);
+    console.log('Liste des suppléments:', this.supplements);
+  }
+
+  removeSupplement(index: number): void {
+    // Logique pour retirer un supplément
+    console.log('Supplément retiré : n°', index);
+    this.supplements.splice(index, 1);
+    console.log('Liste des suppléments:', this.supplements);
+  }
+
+  AddToCart(pizza: Pizza, quantity: number, addedSupplements: Ingredient[], removedIngredients: Ingredient[]): void {
+    // Logique pour ajouter la pizza au panier
+    console.log('Pizza ajoutée au panier:', pizza);
+    console.log('Quantité:', quantity);
+    console.log('Suppléments ajoutés:', addedSupplements);
+    console.log('Ingrédients retirés:', removedIngredients);
   }
 }
