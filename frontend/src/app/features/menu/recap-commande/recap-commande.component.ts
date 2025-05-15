6ec57d44851b4975dc2607ada71aed5bca439271
Tizmax <matthis.bernardini@gmail.com>
2025-05-15 22:02:08 +0200

    affichage du panier fonctionnel

--- frontend/src/app/features/menu/recap-commande/recap-commande.component.ts
+++ frontend/src/app/features/menu/recap-commande/recap-commande.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
+import { CartItem } from '../../../shared/models/cart-item.model';
+import { Pizza } from '../../../shared/models/pizza.model';
+import { Ingredient } from '../../../shared/models/ingredient.model';
 
 @Component({
   selector: 'app-recap-commande',
@@ -7,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrl: './recap-commande.component.css'
 })
 export class RecapCommandeComponent {
+  
+  cartItems: CartItem[] = [];
+  // totalPrice: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getCartItems();
+    // this.loadCartData(); // Charger les données initialement
+  }
+
+  loadCartData(): void {
+    this.cartItems = this.cartService.getCartItems();
+    // this.totalPrice = this.cartService.getCartTotalPrice();
+  }
 
 }
