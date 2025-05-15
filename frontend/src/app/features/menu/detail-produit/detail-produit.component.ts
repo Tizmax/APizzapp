564caa652e6637479b9139e4275846712362c2d3
Tizmax <matthis.bernardini@gmail.com>
2025-05-15 10:18:28 +0200

    Récupère correctement la liste des ingrédients de base et l'affiche

--- frontend/src/app/features/menu/detail-produit/detail-produit.component.ts
+++ frontend/src/app/features/menu/detail-produit/detail-produit.component.ts
@@ -13,22 +13,35 @@ import { Ingredient } from '../../../shared/models/ingredient.model';
 export class DetailProduitComponent {
   pizza: Pizza | null = null;
   ingredients: Ingredient[] = [];
+  ingredientsBase: Ingredient[] = [];
   supplements: Ingredient[] = [];
 
   constructor(private route: ActivatedRoute, private pizzaService: PizzaService) {}
 
   ngOnInit(): void {
+
     const id = this.route.snapshot.paramMap.get('id');
 
     this.pizzaService.getPizzaById(id).subscribe(
-      (data) => this.pizza = data
+      (data) => {this.pizza = data; console.log('Détails de la pizza:', this.pizza)
+      }
     );
 
+  
     
     this.pizzaService.getAllIngredients().subscribe(
       (data) => this.ingredients = data
     );
   }
+
+
+  removeIngredient(index: number): void {
+    // Logique pour retirer un supplément
+    console.log('Ingrédients de base retiré : n°', index);
+    this.ingredientsBase.splice(index, 1);
+    console.log('Liste des ingrédients:', this.ingredientsBase);
+  }
+
 
   addSupplement(ingredient: Ingredient): void {
     // Logique pour ajouter un supplément
@@ -46,7 +59,7 @@ export class DetailProduitComponent {
 
   AddToCart(pizza: Pizza, quantity: number, addedSupplements: Ingredient[], removedIngredients: Ingredient[]): void {
     // Logique pour ajouter la pizza au panier
-    console.log('Pizza ajoutée au panier:', pizza);
+    console.log('Pizza ajo*utée au panier:', pizza);
     console.log('Quantité:', quantity);
     console.log('Suppléments ajoutés:', addedSupplements);
     console.log('Ingrédients retirés:', removedIngredients);
