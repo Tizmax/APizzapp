d475dfe38ef5a42a9592f64ab2854dab07b2c4fb
Tizmax <matthis.bernardini@gmail.com>
2025-05-21 10:55:24 +0200

    User bien utilisé pour passer commande

--- frontend/src/app/features/menu/recap-commande/recap-commande.component.ts
+++ frontend/src/app/features/menu/recap-commande/recap-commande.component.ts
@@ -4,6 +4,7 @@ import { CartService } from '../../../services/cart.service';
 import { PizzaService } from '../../../services/pizza.service';
 import { CartItem } from '../../../shared/models/order.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-recap-commande',
@@ -16,7 +17,7 @@ export class RecapCommandeComponent implements OnInit{
   cartItems: CartItem[] = [];
   // totalPrice: number = 0;
 
-  constructor(private cartService: CartService, private pizzaService: PizzaService, private router: Router, private route: ActivatedRoute, private fb: FormBuilder) {}
+  constructor(private cartService: CartService, private pizzaService: PizzaService, private router: Router, private route: ActivatedRoute, private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.loadCartData(); // Charger les données initialement
@@ -36,7 +37,7 @@ export class RecapCommandeComponent implements OnInit{
   cancelOrder(): void {
     this.cartService.clearCart();
     this.loadCartData(); 
-    this.goToMenu();
+    this.router.navigate(['/dashboards']);
   }
 
   validateOrder(): void {
@@ -53,12 +54,21 @@ export class RecapCommandeComponent implements OnInit{
     //   const supplementsPrice = item.addedSupplements.reduce((sum, s) => sum + s.price, 0);
     //   return total + (item.pizza.basePrice + supplementsPrice) * item.quantity;
     // }, 0);
+    let userId;
+    if (this.authService.isLoggedIn) {
+      // Si l'utilisateur est connecté, on peut récupérer son ID
+      userId = this.authService.currentUserValue?.id;
+    } else {
+      // Sinon, on peut gérer le cas où l'utilisateur n'est pas connecté
+      userId = 1;
+      console.warn('Utilisateur non connecté. ID utilisateur non ajouté aux articles du panier.');
+    }
 
     const order = {
       orderDate: new Date().toISOString(), // ou this.orderForm.value.time
       status: 'PENDING',
       totalAmount: 0,
-      userId: 1,
+      userId: userId,
       orderItems: cartItems
     };
 
